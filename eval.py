6197045f98412910f19921870bd6d66869f01e14
andrasreka <reka@mollia.hu>
2022-12-04 20:53:10 +0200

    eval print

--- eval.py
+++ eval.py
@@ -3,9 +3,10 @@ import sklearn
 import pandas as pd
 
 model = ClassificationModel(
-    "roberta", "models/twitter_roberta_base_irony_plus_mustard",
+    "roberta", "models/twitter_roberta_base_irony_mustard",
     use_cuda=False
 )
 
 eval_df = pd.read_csv('data/val_MUSTARD.csv')
-model.eval_model(eval_df, acc=sklearn.metrics.accuracy_score)
+result, model_outputs, wrong_predictions = model.eval_model(eval_df, acc=sklearn.metrics.accuracy_score)
+print(result)
