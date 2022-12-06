ca538d0d99a3ac7885cdf4121282eeb29440d4a0
Andras Reka <reka@mollia.hu>
2022-12-06 09:16:07 +0000

    new comments

--- comments.py
+++ comments.py
@@ -6,13 +6,15 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 comments = [
     "This is a lovely book. I would recommend it to anyone",
-    "This book is borring. Just a bunch of numbers",
-    "This is, without a doubt, a better story than twilight",
-    "A great read. Captivating. I couldn’t put it down anymore, when I have found out that 0.629 is there"
+    "This book is just a list of numbers. Useless.",
+    "OMG, what a suspenseful read",
+    "More interesting than the Bible",
+    "This is, without a doubt, a more touching story than twilight",
+    "A great read. Captivating. I couldn’t put it down anymore, when I have found out that 0.629 is there",
 ]
 
 model = ClassificationModel(
-    "roberta", "/outputs_reddit_based_on_twitter/checkpoint-24000", use_cuda=False
+    "roberta", "outputs_reddit_based_on_twitter/checkpoint-24000", use_cuda=False
 )
 pred, _ = model.predict(comments)
 print(pred)
